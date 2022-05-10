51365bdc446a832c018ed6f893cad89bedc94a4a
Aven910126 <a1148370488@gmail.com>
2022-05-10 20:49:31 +0800

    first commit

---
+++ src/main/java/com/example/GuideCane/GuideCaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.GuideCane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideCaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideCaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GuideCane/GuideCaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GuideCane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideCaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
